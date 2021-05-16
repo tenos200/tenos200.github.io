403a7da9c991b76de4efcf2c61059e94ee35b494
Tim Enos CS2019 <tim.enos.2020@uni.strath.ac.uk>
2021-05-16 13:16:47 +0100

    reversed to earlier version

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,8 @@
+const openModalBtn = document.querySelectorAll('[data-modal-target]');
+const closeModalBtn = document.querySelectorAll('[data-modal-close]');
+const overlay = document.getElementById('overlay');
+
+
 window.addEventListener('scroll', scrolling);
 
 openModalButtons.foreach(button=> {
@@ -9,15 +14,44 @@ openModalButtons.foreach(button=> {
 	})
 });
 
+
+closeModalButtons.foreach(button=> {
+
+	button.addEventListener('click', () => {
+		const modal = button.closest('.modal');
+		closeModal(modal);
+
+	})
+});
+
+
 function scrolling() {
 	let header = document.querySelector('header');
 	header.classList.toggle('scrolling-active', window.scrollY > 0);
 
 }
 
+function openModal(modal) {
+	if (modal == null) {
+		return;
+	}
+	modal.classList.add('active');
+	overlay.classList.add('active');
+
+}
+
+function closeModal(modal) {
+	if (modal == null) {
+		return;
+	}
+	modal.classList.remove('active');
+	overlay.classList.remove('active');
+
+}
+
 function scrollToTim() {
 	window.scrollTo({
-		top: 930,
+		top: 1000,
 		behavior: 'smooth'
 	});
 }
